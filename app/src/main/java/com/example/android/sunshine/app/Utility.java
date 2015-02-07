c728e5cbce028b78d2f161a9f736da78c820dc1c
Ken Durkin <kdurkin6@fordham.edu>
2015-02-07 00:38:38 -0500

    Update detail fragment UI

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -14,40 +14,35 @@ public class Utility {
         return prefs.getString(context.getString(R.string.pref_location_key),
                 context.getString(R.string.pref_location_default));
     }
-
     public static boolean isMetric(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_units_key),
                 context.getString(R.string.pref_units_metric))
                 .equals(context.getString(R.string.pref_units_metric));
     }
-
-    static String formatTemperature(double temperature, boolean isMetric) {
+    static String formatTemperature(Context context, double temperature, boolean isMetric) {
         double temp;
-        if (!isMetric) {
-            temp = 9 * temperature / 5 + 32;
+        if ( !isMetric ) {
+            temp = 9*temperature/5+32;
         } else {
             temp = temperature;
         }
-        return String.format("%.0f", temp);
+        return context.getString(R.string.format_temperature, temp);
     }
-
     static String formatDate(String dateString) {
         Date date = WeatherContract.getDateFromDb(dateString);
         return DateFormat.getDateInstance().format(date);
     }
-
     // Format used for storing dates in the database. ALso used for converting those strings
 // back into date objects for comparison/processing.
     public static final String DATE_FORMAT = "yyyyMMdd";
-
     /**
      * Helper method to convert the database representation of the date into something to display
      * to users. As classy and polished a user experience as "20140102" is, we can do better.
      *
      * @param context Context to use for resource localization
      * @param dateStr The db formatted date string, expected to be of the form specified
-     *                in Utility.DATE_FORMAT
+     * in Utility.DATE_FORMAT
      * @return a user-friendly representation of the date.
      */
     public static String getFriendlyDayString(Context context, String dateStr) {
@@ -83,14 +78,13 @@ public class Utility {
             }
         }
     }
-
     /**
      * Given a day, returns just the name to use for that day.
      * E.g "today", "tomorrow", "wednesday".
      *
      * @param context Context to use for resource localization
      * @param dateStr The db formatted date string, expected to be of the form specified
-     *                in Utility.DATE_FORMAT
+     * in Utility.DATE_FORMAT
      * @return
      */
     public static String getDayName(Context context, String dateStr) {
@@ -123,13 +117,11 @@ public class Utility {
             return "";
         }
     }
-
     /**
      * Converts db date format to the format "Month day", e.g "June 24".
-     *
      * @param context Context to use for resource localization
      * @param dateStr The db formatted date string, expected to be of the form specified
-     *                in Utility.DATE_FORMAT
+     * in Utility.DATE_FORMAT
      * @return The day in the form of a string formatted "December 6"
      */
     public static String getFormattedMonthDay(Context context, String dateStr) {
@@ -143,5 +135,36 @@ public class Utility {
             e.printStackTrace();
             return null;
         }
+    }
+    public static String getFormattedWind(Context context, float windSpeed, float degrees) {
+        int windFormat;
+        if (Utility.isMetric(context)) {
+            windFormat = R.string.format_wind_kmh;
+        } else {
+            windFormat = R.string.format_wind_mph;
+            windSpeed = .621371192237334f * windSpeed;
+        }
+// From wind direction in degrees, determine compass direction as a string (e.g NW)
+// You know what's fun, writing really long if/else statements with tons of possible
+// conditions. Seriously, try it!
+        String direction = "Unknown";
+        if (degrees >= 337.5 || degrees < 22.5) {
+            direction = "N";
+        } else if (degrees >= 22.5 && degrees < 67.5) {
+            direction = "NE";
+        } else if (degrees >= 67.5 && degrees < 112.5) {
+            direction = "E";
+        } else if (degrees >= 112.5 && degrees < 157.5) {
+            direction = "SE";
+        } else if (degrees >= 157.5 && degrees < 202.5) {
+            direction = "S";
+        } else if (degrees >= 202.5 && degrees < 247.5) {
+            direction = "SW";
+        } else if (degrees >= 247.5 && degrees < 292.5) {
+            direction = "W";
+        } else if (degrees >= 292.5 || degrees < 22.5) {
+            direction = "NW";
+        }
+        return String.format(context.getString(windFormat), windSpeed, direction);
     }
 }
